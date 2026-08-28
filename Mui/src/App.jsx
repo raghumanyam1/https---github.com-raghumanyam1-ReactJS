f2fa50dbdc1047332d37406281549ba27fe0c40a
raghu <raghumsmp@gmail.com>
2026-08-28 17:59:36 +0530

    material ui

---
+++ Mui/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Registration from './Pages/Registration'
+
+function App() {
+
+  return (
+    <>
+      <Registration />
+    </>
+  )
+}
+
+export default App
---
+++ Mui/src/Pages/Registration.jsx
@@ -0,0 +1,30 @@
+import { Typography,Paper,Box } from '@mui/material';
+import React from 'react';
+
+function Registration()
+{
+    return(
+        <>  
+         
+         <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+      
+      }}
+    >
+      
+      <Paper elevation={6} >
+         <Typography variant='h2'>Registration  page</Typography>
+           <Typography variant='body1'>Registration  page</Typography>
+           
+        
+        
+        
+        </Paper>
+    </Box>
+        </>
+    );
+} 
+
+export default Registration; 
