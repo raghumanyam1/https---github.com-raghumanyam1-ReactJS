1f8b5b0560a83da86f2d4865959d4090e3869792
raghu <raghumsmp@gmail.com>
2026-08-31 17:58:04 +0530

    material ui

--- Mui/src/Pages/Registration.jsx
+++ Mui/src/Pages/Registration.jsx
@@ -1,11 +1,25 @@
-import { Typography,Paper,Box } from '@mui/material';
+import { Typography,Paper,Box,Button, ButtonGroup} from '@mui/material';
+import SaveIcon from '@mui/icons-material/Save';
 import React from 'react';
 
 function Registration()
 {
     return(
         <>  
-         
+         <ButtonGroup size= "large" variant="contained" aria-label="Basic button group">
+      <Button>One</Button>
+      <Button>Two</Button>
+      <Button>Three</Button>
+    </ButtonGroup>
+
+     <ButtonGroup variant="outlined" aria-label="Loading button group">
+      <Button>Submit</Button>
+      <Button>Fetch data</Button>
+      <Button loading loadingPosition="start" startIcon={<SaveIcon />}>
+        Save
+      </Button>
+    </ButtonGroup>
+
          <Box
       sx={{
         display: 'flex',
@@ -17,10 +31,6 @@ function Registration()
       <Paper elevation={6} >
          <Typography variant='h2'>Registration  page</Typography>
            <Typography variant='body1'>Registration  page</Typography>
-           
-        
-        
-        
         </Paper>
     </Box>
         </>
